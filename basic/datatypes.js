a2f95836777478798eb955bec08430c97b207819
artistof <154748949+artistof@users.noreply.github.com>
2024-05-06 07:29:29 +0000

    up to 5

---
+++ basic/datatypes.js
@@ -0,0 +1,18 @@
+//console.log("hello world");
+
+let name="romeo";
+
+let age=52
+
+let is_logged_in = false
+
+
+
+//null is a standalone value. a type
+//undefined 
+
+console.log(typeof age);
+
+
+
+
